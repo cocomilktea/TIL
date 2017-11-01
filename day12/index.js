62f3f8d7fadbf8def2cc863d95a6e618bcdb3b05
cocomilktea <coco_milktea@naver.com>
2017-11-01 22:49:37 +0900

    coding practice

--- day12/index.js
+++ day12/index.js
@@ -1,1 +1,16 @@
 document.getElementById('main').innerHTML =  "Hello, js";
+var btn = document.createElement("BUTTON");
+
+$(document).ready(function(){
+    $("p").click(function(){
+        $(this).hide();
+    });
+});
+
+for(var i = 1; i < 27; i++){
+	var btn = document.createElement("BUTTON");
+	btn.id = btn+i;
+	btn.innerHTML = i;
+	document.body.appendChild(btn);
+};
+
