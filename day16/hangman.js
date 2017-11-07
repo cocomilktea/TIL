a94e69943edd1ff9e4087a0047ac78244fc3dde0
cocomilktea <coco_milktea@naver.com>
2017-11-07 22:33:22 +0900

    행맨 게임 예제 코드 설명 추가

--- day16/hangman.js
+++ day16/hangman.js
@@ -142,7 +142,7 @@ game.updateDisplay();
 */
 
 
-//layout
+//get object from html
 var divLife = document.getElementById('life');
 var divMesg = document.getElementById('message');
 var keys = document.getElementById('keys');
@@ -150,20 +150,23 @@ var showButton = document.getElementById('hint_button');
 var hintMessage = document.getElementById('hint_msg');
 
 
+//add toggle button
+//show hint and hide hint
 var hintToggle = false;
-
 showButton.onclick = function() {
-  if (!hintToggle) {
-    hintMessage.innerHTML = game.hint;
-    hintToggle = true;
-  } else {
-    hintMessage.innerHTML = "";
+  if (hintToggle) {
     hintToggle = false;
+    hintMessage.innerHTML = "";
+  } else {
+    hintToggle = true;
+    hintMessage.innerHTML = game.hint;
   }
-}
+};
 
+//add button from a to z
+//add <br> at m
 for (var i = 'a'.charCodeAt(0); i <= 'z'.charCodeAt(0); i++ ) {
-  var str = String.fromCharCode(i);
+  var str = String.fromCharCode(i);  //String 문자열에 관한 static메소드(ex Math.random과 같은 예)
   var button = document.createElement('button');
   button.innerHTML = str;
   button.className += "alphabet";
@@ -173,17 +176,25 @@ for (var i = 'a'.charCodeAt(0); i <= 'z'.charCodeAt(0); i++ ) {
   }
 }
 
+//add event handler to keys div
+//이 코드는 버튼을 누른 후에 실행됩니다.
+//게임 초기화 코드가 먼저 실행됨
 keys.onclick = function(event) {
+  console.log(event.target)
   //outside button
   if (!!event.target.id) {
     console.log("outside");
     return;
   }
+
+  //finallay, button clicked!
   var x = event.target.innerHTML;
-  console.log(x);
+  //console.log(x);
+  //update game status
   game.checkWords(x);
-  game.updateDisplay();
-}
+  //update html elements
+  updateDisplay();
+};
 
 
 
@@ -191,11 +202,15 @@ keys.onclick = function(event) {
 var game = {life: 5, count: 0};
 
 game.init = function() {
-  var f = "javascript js;swift ios;starbucks siren;google search;apple stevejobs;codesquad bestAcademy;developer dream";
-  var farr = f.split(";");
-
+  var f = "apple#red;banana#yellow;kiwy#green;watermelon#줄무늬 과일;strawberry #red";
+  var farr = f.split(";"); //farr[0] = "apple#red"
   var idx = Math.floor(Math.random() * farr.length);
-  [this.answer, this.hint] = farr[idx].split(" ");
+  //[this.answer, this.hint] = farr[idx].split("#");
+  var ans_arr = farr[idx].split("#"); //공백보단 평소에 안쓰는걸 구분자로 쓰는게 좋다
+  this.answer = ans_arr[0];
+  this.hint = ans_arr[1];
+
+  //for user input
   this.guess = [];
   for (var i = 0; i < this.answer.length; i++) {
     this.guess.push('_');
@@ -204,36 +219,53 @@ game.init = function() {
 game.init();
 console.log(game);
 
+game.correct = function(i, letter) {
+  this.count++;
+  this.guess[i] = letter;
+};
 
+game.incorrect = function() {
+  this.life--;
+};
+
+//update game status when user input
 game.checkWords = function(letter) {
   var find = false;
   var ans = this.answer;
   for (var i = 0; i < ans.length; i++) {
     //맞췄다.
     if (ans[i] === letter) {
-      game.count++;
-      game.guess[i] = letter;
+      game.correct(i, letter);
       find = true;
     }
   }
   //못 찾았다면
   if (!find) {
-    game.life--;
+    game.incorrect();
   }
 };
 
 game.showResult = function() {
   return game.guess.join(" ");
-}
+};
 
-game.updateDisplay = function() {
+game.isOver = function() {
+  return this.life === 0;
+};
+
+game.isClear = function() {
+  return this.answer.length == this.count;
+};
+
+
+var updateDisplay = function() {
   updateLife();
   updateMesg();
-  if (this.life === 0) {
-    this.gameOver();
+  if (game.isOver()) {
+    game.gameOver();
   }
-  if (this.count === this.answer.length) {
-    this.clear();
+  if (game.isClear()) {
+    game.clear();
   }
 };
 
@@ -243,10 +275,6 @@ var updateLife = function() {
 var updateMesg = function() {
   divMesg.innerHTML = game.showResult();
 };
-
-var showHint = function() {
-  alert("Hint:" + answer.hint);
-}
 
 game.clear = function() {
   keys.innerHTML = "Cogratulation!";
@@ -260,14 +288,7 @@ game.gameOver = function() {
   document.getElementById('hint').remove();
 }
 
-game.updateDisplay();
-
-
-
-
-
-
-
+updateDisplay();
 
 
 
